8e38d67528bdd65487dd1946ea4bc2f31b7d3d40
Rich <richard@flagship.co>
2011-03-22 11:15:45 +0400

    Initial commit of couch tweet code - just a Hello World atm

---
+++ src/rit/gtug/dubai/HackForJapanServlet.java
@@ -0,0 +1,13 @@
+package rit.gtug.dubai;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HackForJapanServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
